27e145840776ac522f3374b22ec825d9753e817c
padraigobrien <padraig@padraigobrien.com>
2016-05-09 17:31:51 +0100

    _

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var multer = require('multer');
 var fs = require('fs');
 var app = express();
+var Q = require('q');
 
 var storage = multer.diskStorage({
-
     destination: function (req, file, callback) {
         callback(null, './uploads');
     },
@@ -13,19 +13,20 @@ var storage = multer.diskStorage({
     }
 });
 
-var upload = multer({ storage: storage });
-
-var index = 0;
+var upload = multer({storage: storage});
 
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-var upload = multer({ storage: storage });
+var upload = multer({storage: storage});
+
+var index = 0;
+var array=[];
 
 function cleanUpFile(buffer) {
     var array = buffer.toString().replace(/(\r\n|\n|\r)/gm, "").replace(/ /g, '');
     return array;
 }
-function processEachBatch(callback) {
+function processEachBatch() {
 
     numberOfPaintColors = array[++index];
     matted = [];
@@ -78,25 +79,28 @@ function processEachBatch(callback) {
             }
         }
         if (!satisfied) {
-            callback("Impossible");
+            return "Impossible";
         }
         else {
-            return callback(matted);
+            return matted;
 
         }
 
     }
 }
-function processAllBatchs(numberOfTestCases){
-    for (i = 0; i < numberOfTestCases; i++) {
-        result =processEachBatch();
-        console.log("case number "+i+" " +result);
 
-    }
-}
-function returnResults(err,data){
-    console.log("data"+ data)
+var readFile = function (path, callback) {
+    var deferred = Q.defer();
+    fs.readFile(path, function (err, data) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve(data);
+        }
+    })
+    return deferred.promise;
 };
+
 
 app.get('/', function (req, res) {
 
@@ -106,12 +110,27 @@ app.get('/', function (req, res) {
 
 app.post('/uploads', upload.single('file'), function (req, res, next) {
 
-    fs.readFile(req.file.path, function (err, data) {
-        if (err) console.log(err);
-        console.log("uploading");
+    var results=[];
+    index=0;
+
+    readFile(req.file.path)
+        .then(function(result){
+            return cleanUpFile(result.toString());
+        })
+        .then(function(result){
+            array=result;
+            numberOfTestCases = array[index];
+            for (i = 0; i < numberOfTestCases; i++) {
+                results.push("use case " + i + ": " + processEachBatch());
+            }
+           return results;
+        })
+        .then(function(result){
+            console.log(result)
+        })
+
         res.send();
         res.status(200).end();
-    });
 });
 
 app.listen(8080, function () {
